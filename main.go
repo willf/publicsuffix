06465ba9684cf90094dd948e81ae7d71cfd81544
Will Fitzgerald <37049+willf@users.noreply.github.com>
2023-09-12 18:17:18 -0400

    more error handling

--- main.go
+++ main.go
@@ -3,21 +3,87 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
-	"strings"
+	"regexp"
 
 	"github.com/weppos/publicsuffix-go/publicsuffix"
 )
 
+type NullableString string
+
+func (s NullableString) MarshalJSON() ([]byte, error) {
+	if s == "" {
+		return []byte("null"), nil
+	}
+	return json.Marshal(string(s))
+}
+
 type Result struct {
-	Input       string `json:"input"`
-	BasicDomain string `json:"base"`
-	TopLevel    string `json:"tld"`
-	SecondLevel string `json:"sld"`
-	ThirdLevel  string `json:"trd"`
-	Error       error  `json:"error"`
+	Input        string         `json:"input"`
+	BasicDomain  string         `json:"base"`
+	TopLevel     string         `json:"tld"`
+	SecondLevel  string         `json:"sld"`
+	ThirdLevel   string         `json:"trd"`
+	ErrorMessage NullableString `json:"error"`
+}
+
+func FinalCheck(tld string, sld string, trd string) (err error) {
+	if tld == "" {
+		return errors.New("tld is empty")
+	}
+	if sld == "" {
+		return errors.New("sld is empty")
+	}
+	// make sure there are no extraneous things in the tld or the sld
+	// regular expression for tld is: ^[a-z0-9\-]+$
+	regexTld := regexp.MustCompile(`^[a-z0-9\-\.]+$`)
+	if !regexTld.MatchString(tld) {
+		return errors.New("tld contains invalid characters")
+	}
+	// regular expression for sld is: ^[a-z0-9\-]+$
+	regexSld := regexp.MustCompile(`^[a-z0-9\-]+$`)
+	if !regexSld.MatchString(sld) {
+		return errors.New("sld contains invalid characters")
+	}
+	// regular expression for trd is: ^[a-z0-9\-]+$
+	regexTrd := regexp.MustCompile(`^[a-z0-9\-]+$`)
+	if trd != "" && !regexTrd.MatchString(trd) {
+		return errors.New("trd contains invalid characters")
+	}
+	return nil
+}
+
+func ParseHost(input string, host string) (result Result) {
+	parsed, err := publicsuffix.Parse(host)
+	if err != nil {
+		return Result{Input: input, ErrorMessage: NullableString(err.Error())}
+	} else {
+		tld := parsed.TLD
+		sld := parsed.SLD
+		trd := parsed.TRD
+		sldPlusTld := sld + "." + tld
+		finalCheck := FinalCheck(tld, sld, trd)
+		if finalCheck != nil {
+			return Result{Input: input, ErrorMessage: NullableString(finalCheck.Error())}
+		}
+		return Result{Input: input, BasicDomain: sldPlusTld, TopLevel: tld, SecondLevel: sld, ThirdLevel: trd}
+	}
+}
+
+func Parse(input string) (result Result) {
+	u, err := url.Parse(input)
+	if err == nil && u.Host != "" {
+		return ParseHost(input, u.Host)
+	} else {
+		u, err = url.Parse("https://" + input)
+		if err == nil && u.Host != "" {
+			return ParseHost(input, u.Host)
+		}
+	}
+	return ParseHost(input, input)
 }
 
 func main() {
@@ -30,32 +96,7 @@ func main() {
 
 	for scanner.Scan() {
 		input := scanner.Text()
-		domainName := input
-
-		// Parse the input as a URL
-		u, err := url.Parse(domainName)
-		if err == nil && u.Host != "" {
-			domainName = u.Host
-		} else {
-			u, err = url.Parse("https://" + domainName)
-			if err == nil && u.Host != "" {
-				domainName = u.Host
-			}
-		}
-
-		parsed, err := publicsuffix.Parse(domainName)
-		if err != nil {
-			result := Result{Input: domainName, Error: err}
-			jsonData, _ := json.Marshal(result)
-			fmt.Println(string(jsonData))
-			continue
-		}
-		tld := strings.Trim(parsed.TLD, "/")
-		sld := parsed.SLD
-		trd := parsed.TRD
-		sldPlusTld := sld + "." + tld
-
-		result := Result{Input: input, BasicDomain: sldPlusTld, TopLevel: tld, SecondLevel: sld, ThirdLevel: trd, Error: err}
+		result := Parse(input)
 		jsonData, _ := json.Marshal(result)
 		fmt.Println(string(jsonData))
 	}
